634615f31f62e8f79ec4e1dbc284442499a169dd
kellydavid <kellydavid178@gmail.com>
2015-10-31 11:40:06 +0000

    Improved compile script

--- lab2/compile.sh
+++ lab2/compile.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-mkdir bin
+
+# Create bin directory if it doesn't already exist
+if [ ! -d "bin/" ]; then
+        mkdir bin
+fi
+
+# Compile source files into bin directory
 javac -d bin/ src/lab2/*.java
